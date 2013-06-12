ee2bce7887afeb0af9e46c3e4f142e8eac3c069a
Felipe Ismael Barth <fibbarth@gmail.com>
2013-06-12 15:57:22 -0300

    Update retira_shortags.php

--- retira_shortags.php
+++ retira_shortags.php
@@ -1,8 +1,11 @@
 // Retira as short tags
-   function clearShortTags( $arquivo ){
-     	$pattern = '/<\?(?!php)(?!xml)=?/';
-   		if( preg_match($pattern, $content ) ){
-   			$content = preg_replace($pattern, '<?php ', $content );
+   function clearShortTags( $content, $arquivo ){
+      	$patternTag  = '/(<\?(?!php)(?!xml)(?!=))/';
+   		$patternEcho = '/(<\?=)/';
+   		$pattern = array( $patternTag, $patternEcho);
+   		$replace = array( '<?php ', '<?php echo ');
+   		if( preg_match($patternTag, $content ) || preg_match($patternEcho, $content ) ){
+   			$content = preg_replace($pattern, $replace, $content );
    			file_put_contents($arquivo, $content, LOCK_EX);
    		}
    }
